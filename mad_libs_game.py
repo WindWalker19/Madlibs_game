eeda8ffbb8b227832a776fb42bb88f676e46e872
Sonam sherpa <sherpasonam44@hotmail.com>
2020-06-05 17:49:54 -0700

    Add files via upload

---
+++ mad_libs_game.py
@@ -0,0 +1,14 @@
+#MadLibs.
+
+print("Roses are red.\nViolets are blue.\nI love you.")
+
+#We replace the red,Violets and you by the input that we provide.
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity name: ")
+print("\n")
+print("Below is the new story line.")
+print("Roses are",color,".")
+print(plural_noun,"are blue.")
+print("I love",celebrity,".")
